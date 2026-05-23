13a80a10bb932f5d280768d78dcbbd762b332e94
sulivan-moreau <sulivan.moreau@laplateforme.io>
2026-05-23 17:07:09 +0200

    Lazy load model in WaterPredictor to fix CI without model.pkl

--- api/services/predictor.py
+++ api/services/predictor.py
@@ -5,9 +5,15 @@ from api.schemas.water import WaterInput, PredictionOutput
 class WaterPredictor:
 
     def __init__(self, model_path: str):
-        self.model = joblib.load(model_path)
+        self.model_path = model_path
+        self.model = None
+
+    def _load(self):
+        if self.model is None:
+            self.model = joblib.load(self.model_path)
 
     def predict(self, water: WaterInput) -> PredictionOutput:
+        self._load()
         features = np.array([[
             water.ph,
             water.hardness,
